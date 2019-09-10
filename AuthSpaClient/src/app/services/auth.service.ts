dcb64c0d2377b765a0c20ccebccd818c8e7efc81
FoxGeoff <fox.geoffrey@gmail.com>
2019-09-10 13:45:12 -0700

    Add links on toolbar

--- AuthSpaClient/src/app/services/auth.service.ts
+++ AuthSpaClient/src/app/services/auth.service.ts
@@ -35,7 +35,6 @@ export class AuthService {
   completeAuthentication(): Promise<void> {
     return this.manager.signinRedirectCallback().then(user => {
       this.user = user;
-      console.log(user);
     });
   }
 }
